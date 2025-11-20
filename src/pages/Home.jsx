b616c32a142f3843e9299d486ed1e187cfe7fcbe
Nguyễn Minh Khang <thanhthanthiendia12@gmail.com>
2025-11-20 18:20:47 +0700

    change brand

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -197,7 +197,7 @@ const Home = () => {
           >
             <div className="relative z-10">
               <img 
-                src="/img/techingheader.png"
+                src="/public/icons/VietLearn.png"
                 alt="Online Learning"
                 className="w-full max-w-lg mx-auto drop-shadow-2xl"
               />
